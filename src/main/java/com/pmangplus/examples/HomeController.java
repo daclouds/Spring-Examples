2dad529ce947aa7c7eb6bc303cf613b73f143f62
daclouds <daclouds@gmail.com>
2013-01-17 13:05:48 +0900

    added MappingJacksonHttpMessageConvertor

--- src/main/java/com/pmangplus/examples/HomeController.java
+++ src/main/java/com/pmangplus/examples/HomeController.java
@@ -2,7 +2,10 @@ package com.pmangplus.examples;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pmangplus.examples.domain.Person;
+import com.pmangplus.examples.repository.PersonRepository;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +25,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Inject
+	PersonRepository repository;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,6 +44,11 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public @ResponseBody List<Person> list() {
+		return repository.findAll();
 	}
 	
 }
